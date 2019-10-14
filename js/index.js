1e7f0b2967980ebc82c943239008669d4c82d387
Harvey <laozhaochaguan@gmail.com>
2019-10-14 10:51:35 +0800

    add font

--- js/index.js
+++ js/index.js
@@ -4,5 +4,13 @@ var ctx = $canvas.getContext("2d");
 var img = new Image();
 img.src = "./img/style01_bg.webp";
 img.onload = function() {
-  ctx.drawImage(this, 0, 0);
+  setTimeout(() => {
+    ctx.drawImage(this, 0, 0);
+    ctx.font = "60px pixel";
+    ctx.fillStyle = "#000";
+    ctx.globalAlpha = 0.7;
+    ctx.translate($canvas.width / 2, $canvas.height / 2);
+    ctx.rotate((Math.PI / 180) * 9.5);
+    ctx.fillText("汉体书写", -320, -80);
+  }, 2000);
 };
