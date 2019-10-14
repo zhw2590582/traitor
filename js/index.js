8414ce53f08043d60cba5c3054e9f11f517e5a5d
Harvey <laozhaochaguan@gmail.com>
2019-10-14 11:28:45 +0800

    change font

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
 var $canvas = document.querySelector("canvas");
 var ctx = $canvas.getContext("2d");
 
-var img = new Image();
-img.src = "./img/style01_bg.webp";
-img.onload = function() {
+var bgImg = new Image();
+bgImg.src = "./img/style01_bg.webp";
+bgImg.onload = function() {
   setTimeout(() => {
-    ctx.drawImage(this, 0, 0);
-    ctx.font = "60px pixel";
-    ctx.fillStyle = "#000";
-    ctx.globalAlpha = 0.7;
-    ctx.translate($canvas.width / 2, $canvas.height / 2);
-    ctx.rotate((Math.PI / 180) * 9.5);
-    ctx.fillText("汉体书写", -320, -80);
-  }, 2000);
+    darw("诺基亚短信");
+  }, 3000);
 };
+
+var test = new Image();
+test.src = "./img/alipay.jpg";
+
+function darw(text) {
+  $canvas.style.letterSpacing = "5px";
+  ctx.clearRect(0, 0, $canvas.width, $canvas.height);
+  ctx.drawImage(bgImg, 0, 0);
+  ctx.font = "60px pixel";
+  ctx.fillStyle = "#000";
+  ctx.translate($canvas.width / 2, $canvas.height / 2);
+  ctx.rotate((Math.PI / 180) * 9.5);
+  ctx.fillText(text, -320, -80);
+  ctx.globalCompositeOperation = "overlay";
+  ctx.globalAlpha = 0.75;
+  ctx.translate(-$canvas.width / 2, -$canvas.height / 2);
+  ctx.rotate(-(Math.PI / 180) * 9.5);
+  ctx.drawImage(bgImg, -90, 80);
+}
