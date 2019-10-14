7a6f5a06b7cf431b68dba518e4860e675c2d3d5e
Harvey <laozhaochaguan@gmail.com>
2019-10-14 11:43:57 +0800

    添加覆盖效果

--- js/index.js
+++ js/index.js
@@ -5,25 +5,28 @@ var bgImg = new Image();
 bgImg.src = "./img/style01_bg.webp";
 bgImg.onload = function() {
   setTimeout(() => {
-    darw("诺基亚短信");
-  }, 3000);
+    darw("诺基亚短信 TEXT");
+  }, 100);
 };
 
-var test = new Image();
-test.src = "./img/alipay.jpg";
+var cover = new Image();
+cover.src = "./img/cover.png";
 
 function darw(text) {
-  $canvas.style.letterSpacing = "5px";
+  $canvas.style.letterSpacing = "6px";
   ctx.clearRect(0, 0, $canvas.width, $canvas.height);
   ctx.drawImage(bgImg, 0, 0);
-  ctx.font = "60px pixel";
-  ctx.fillStyle = "#000";
+  ctx.font = "64px pixel";
+  ctx.fillStyle = "#333";
   ctx.translate($canvas.width / 2, $canvas.height / 2);
   ctx.rotate((Math.PI / 180) * 9.5);
   ctx.fillText(text, -320, -80);
-  ctx.globalCompositeOperation = "overlay";
-  ctx.globalAlpha = 0.75;
+  ctx.font = "60px pixel";
+  ctx.fillStyle = "#76b8d5";
+  ctx.fillText(text.length + '/900', 120, -180);
+  ctx.globalCompositeOperation = "darken";
+  ctx.globalAlpha = 0.3;
   ctx.translate(-$canvas.width / 2, -$canvas.height / 2);
   ctx.rotate(-(Math.PI / 180) * 9.5);
-  ctx.drawImage(bgImg, -90, 80);
+  ctx.drawImage(cover, -90, 80);
 }
