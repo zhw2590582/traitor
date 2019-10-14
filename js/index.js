c6161831a2c4dcd786aa6d0d30cb8278e1b0d127
Harvey <laozhaochaguan@gmail.com>
2019-10-14 11:53:15 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,18 +3,18 @@ var ctx = $canvas.getContext("2d");
 
 var bgImg = new Image();
 bgImg.src = "./img/style01_bg.webp";
-bgImg.onload = function() {
-  setTimeout(() => {
-    darw("诺基亚短信 TEXT");
-  }, 100);
-};
-
 var cover = new Image();
 cover.src = "./img/cover.png";
 
+bgImg.onload = function() {
+  setTimeout(() => {
+    darw("有内鬼终止交易");
+  }, 3000);
+};
+
 function darw(text) {
+  ctx.fillRect(0, 0, $canvas.width, $canvas.height);
   $canvas.style.letterSpacing = "6px";
-  ctx.clearRect(0, 0, $canvas.width, $canvas.height);
   ctx.drawImage(bgImg, 0, 0);
   ctx.font = "64px pixel";
   ctx.fillStyle = "#333";
@@ -23,7 +23,7 @@ function darw(text) {
   ctx.fillText(text, -320, -80);
   ctx.font = "60px pixel";
   ctx.fillStyle = "#76b8d5";
-  ctx.fillText(text.length + '/900', 120, -180);
+  ctx.fillText(text.length + "/900", 120, -180);
   ctx.globalCompositeOperation = "darken";
   ctx.globalAlpha = 0.3;
   ctx.translate(-$canvas.width / 2, -$canvas.height / 2);
